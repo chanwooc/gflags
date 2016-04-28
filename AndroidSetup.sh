6bfc24032402ac322eab6d0901cb7a26873cb211
Chanwoo Chung <cwchung@mit.edu>
2016-04-28 17:30:33 -0400

    android-18 -> android-16, added auto-download of android-cmake

--- AndroidSetup.sh
+++ AndroidSetup.sh
@@ -5,19 +5,25 @@ if [ -z "$NDK" ]; then
 	exit 1
 fi
 
-CMAKE_TOOLCHAIN_FILE=/home/cwchung/workspace/android-cmake/android.toolchain.cmake
+BASEDIR=$(cd $(dirname $0); pwd -P)
+CMAKE_TOOLCHAIN_DIR=$BASEDIR/android-cmake
+
+if [ ! -d $CMAKE_TOOLCHAIN_DIR ]; then
+	( git clone https://github.com/cwchung90/android-cmake; )
+fi
+
+
 mkdir -p build
 cd build
 
-cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE \
+cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_DIR/android.toolchain.cmake \
 	-DANDROID_NDK=$NDK \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DANDROID_ABI="armeabi-v7a with NEON" \
-	-DANDROID_NATIVE_API_LEVEL=18 \
+	-DANDROID_NATIVE_API_LEVEL=16 \
 	-DANDROID_TOOLCHAIN_NAME="arm-linux-androideabi-4.8" \
 	-DANDROID_STL="gnustl_static" \
 	-DGFLAGS_NAMESPACE="google" \
 	..
-
 
 make
